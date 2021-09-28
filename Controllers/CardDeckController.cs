9dd544fc97f9a616d2dfc5d6091b1a6f6b63e12f
Torianna Hudson <toriannahudson@quickenloans.comclear>
2021-09-28 08:47:43 -0400

    final

---
+++ Controllers/CardDeckController.cs
@@ -0,0 +1,44 @@
+﻿using CardLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CardLab.Controllers
+{
+    public class CardDeckController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> shufflecards(int deckcount)
+        {
+            string domain = "https://deckofcardsapi.com/";
+            string path = $"/api/deck/new/shuffle/?deck_count={deckcount}";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path);
+
+            Root result = await connection.Content.ReadAsAsync<Root>();
+            return View(result);
+        }
+
+        public async Task<IActionResult> drawcard(int deckcount)
+        {
+            string domain = "https://deckofcardsapi.com/";
+            string path = $"/api/deck/new/draw/?count={deckcount}";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path);
+
+            Cards result = await connection.Content.ReadAsAsync<Cards>();
+            return View(result);
+        }
+    }
+}
---
+++ Models/Deck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardLab.Models
+{
+    public class Root
+    {
+
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+        public bool shuffled { get; set; }
+        public int deck_count { get; set; }
+        public List<Cards> cards { get; set; }
+    }
+
+    public class Cards
+    {
+        public bool success { get; set; }
+        public List<string> cards { get; set; }
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+    }
+
+}
